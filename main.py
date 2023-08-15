4671eff715cd8a94139439dbafd2bd8f13ef229c
Aman Ray <142166207+AmanRay14@users.noreply.github.com>
2023-08-15 15:34:57 +0530

    Update main.py
    Random Password Generator
    
    This Python program generates a random password based on user-defined criteria.
    The generated password includes a mix of uppercase and lowercase letters,
    digits, and punctuation characters. The user can specify the desired length
    of the password.
    
    To use:
    1. Run the script.
    2. Enter the desired password length.
    3. The program will generate and display a random password.

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ import random
 import string
 
 def generate_password(length=12):
+#Define a function to generate a random password.
+    
+#Define a string containing all possible characters for the password.
     characters = string.ascii_letters + string.digits + string.punctuation
+    
+#Generate a password by selecting random characters from the defined set.
     password = ''.join(random.choice(characters) for _ in range(length))
+    
+#Return the generated password.
     return password
 
 if __name__ == "__main__":
+#This block of code is executed when the script is run.
+    
+    print("Random Password Generator")
+    print("------------------------")
+    
+#Get the desired password length from the user.
     password_length = int(input("Enter the desired password length: "))
+    
+#Call the generate_password function to create a new password.
     new_password = generate_password(password_length)
-    print(f"Generated Password: {new_password}")
+    
+#Display the generated password.
+    print(f"\nGenerated Password: {new_password}")
